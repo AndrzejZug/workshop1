64e0205d00f0c6466ffc3969b364e94d974792eb
AndrzejZug <ajzugaj@gmail.com>
2020-09-26 16:36:32 +0200

    Menu wyboru kompletne;  -dodane metody do wyświetlania listy (wip), opuszczania programu (early), dodawania elementów listy (relCandidate);

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,8 +1,6 @@
 package pl.coderslab;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,11 +11,14 @@ import java.util.Scanner;
 
 public class TaskManager {
 
+    static String[][] newTab;
+
     public static final String FILE_NAME = "storage/tasks.csv";
 
     public static void main(String[] args) {
         //System.out.println(ConsoleColors.BLUE + "Hello, World !!!");
-        readTable(FILE_NAME);
+
+        newTab = readTable(FILE_NAME);
         menuOptions();
         menuChoice();
 
@@ -36,23 +37,23 @@ public class TaskManager {
 
     public static void menuChoice() {
         Scanner scanner = new Scanner(System.in);
-String choice ="";
+        String choice;
 
         while (scanner.hasNextLine()) {
             choice = scanner.nextLine();
             {
                 switch (choice) {
                     case "add":
-                        System.out.println("addTask()");
+                        addTask();
                         break;
                     case "remove":
                         System.out.println("removeTask()");
                         break;
                     case "list":
-                        System.out.println("listTask()");
+                        listTask();
                         break;
                     case "exit":
-                        System.out.println("exit()");
+                        listQuit();
                         break;
 
 // other options
@@ -65,17 +66,68 @@ String choice ="";
         }
     }
 
+    public static void addTask() {
+//        array = Arrays.copyOf(array, len + 1); // copying the array
+//        array[len] = new String[2]; // creating and assigning string array to new row else it will be null
+//        array[len][0] = str
+        newTab = Arrays.copyOf(newTab, newTab.length + 1);
+        newTab[newTab.length - 1] = new String[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please add task description");
+        String tmp = scanner.nextLine();
+        newTab[newTab.length - 1][0] = tmp;
+        System.out.println("Please add task due date");
+        tmp = scanner.nextLine();
+        newTab[newTab.length - 1][1] = tmp;
+        System.out.println("Is your task extra special: true/false");
+        tmp = scanner.nextLine();
+        newTab[newTab.length - 1][2] = tmp;
+//        System.out.print(newTab[newTab.length - 1][0] + " ");
+//        System.out.print(newTab[newTab.length - 1][1] + " ");
+//        System.out.println(newTab[newTab.length - 1][2] + " ");
+//        System.out.print(newTab[newTab.length - 2][0] + " ");
+//        System.out.print(newTab[newTab.length - 2][1] + " ");
+//        System.out.println(newTab[newTab.length - 2][2] + " ");
+        menuOptions();
+
+    }
+
+    public static void listTask() {
+
+        for (int i = 0; i < newTab.length; i++) {
+            for (int j = 0; j < newTab[i].length; j++) {
+                System.out.print(" " + newTab[i][j]);
+            }
+            System.out.println("");
+
+
+        }
+        menuOptions();
+    }
+
+    public static void listQuit() {
+
+        System.out.println(ConsoleColors.RED + "Have a good day :)");
+        System.exit(0);
+
+    }
+
 
     public static String[][] readTable(String fileName) {
-        Path path = Paths.get(FILE_NAME);
         File file = new File(FILE_NAME);
+        Path location = Paths.get(FILE_NAME);
+        String[][] tasks = null;
+        if (!Files.exists(location)) {
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+        try {
+            Scanner scanner = new Scanner(file);
 
-        try (Scanner scanner = new Scanner(file)) {
-
-            long rowCounterLong = Files.lines(path).count();
+            long rowCounterLong = Files.lines(location).count();
             int rowCounter = (int) rowCounterLong;
 
-            String[][] task = new String[rowCounter][3];
+            tasks = new String[rowCounter][3];
 
             int i = 0;
             while (scanner.hasNextLine()) {
@@ -84,28 +136,30 @@ String choice ="";
 
                 for (int j = 0; j < split.length; j++) {
                     //System.out.print(split[j]);
-                    task[i][j] = split[j];
+                    tasks[i][j] = split[j];
 
                 }
                 i++;
             }
+        scanner.close();
 
-//            System.out.println("[0][0]: " + task[0][0]);
-//            System.out.println("[1][1]: " + task[1][1]);
-//            System.out.println("[2][3]: " + task[2][2]);
-            return task;
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException ez) {
-            ez.printStackTrace();
-
-        } finally {
-            return null;
+        } catch (IOException e) {
+            System.out.println("Bład pliku");
         }
-
+        return tasks;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
